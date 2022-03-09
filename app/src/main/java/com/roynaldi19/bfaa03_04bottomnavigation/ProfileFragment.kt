8c4f0d793465274be838edb345a7a4a767ad1ee9
roynaldi19 <roynaldi19@gmail.com>
2022-03-09 11:22:08 +0700

    upddate mar 22

--- app/src/main/java/com/roynaldi19/bfaa03_04bottomnavigation/ProfileFragment.kt
+++ app/src/main/java/com/roynaldi19/bfaa03_04bottomnavigation/ProfileFragment.kt
@@ -6,7 +6,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-
 class ProfileFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
